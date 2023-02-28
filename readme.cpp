3ed541255c2b662d1408ba535dc0a89ae0d733da
tanvi1053 <104745937+tanvi1053@users.noreply.github.com>
2023-02-28 23:39:20 +1030

    Update readme.cpp

--- readme.cpp
+++ readme.cpp
@@ -1,10 +1,9 @@
-
 // test 
 #include <iostream>
 using namespace std;
 
 int main() {
   cout << "Hello World!";
-  cout << "I am learning C++";
+  cout << "I am learning how to use git";
   return 0;
 }
